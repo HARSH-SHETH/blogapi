e0dc0f713b5365a73285e583579e24283ad84bd3
HARSH-SHETH <ycdtharsh@gmail.com>
2020-11-01 16:24:41 +0530

    fixed syntax errors

--- models/blogpost.js
+++ models/blogpost.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const blogPostSchema = new mongoose.Schema({
-  title: { type: String, required, minlength: 5 },
-  content: { type: String, required },
-  timestamp: { type: Date, required },
+  title: { type: String, required:true, minlength: 5 },
+  content: { type: String, required:true },
+  timestamp: { type: Date, required:true },
   comments: [ { type: mongoose.Schema.Types.ObjectId, ref: 'comment' } ],
   isPublished: { type: Boolean, default: false }
 });
--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
-  commentBody: { type: String, required },
+  commentBody: { type: String, required:true },
   commentOwner: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   timestamp: { type: Date },
   upvotes: { type: Number, default: 1 },
