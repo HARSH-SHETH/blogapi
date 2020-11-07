046e8b8144681386029a0a306ca6bb10982620f4
HARSH-SHETH <ycdtharsh@gmail.com>
2020-11-07 13:08:05 +0530

    show all blogposts

--- routes/blogpost.js
+++ routes/blogpost.js
@@ -1,7 +1,15 @@
-var app = require('express').Router();
+const app = require('express').Router();
+const userModel = require('../models/user.js');
+const blogpostModel = require('../models/blogpost.js');
+const commentModel = require('../models/comment.js');
 
 app.get('/', (req, res) => {
-  res.send('welcome to my blog route');
+  blogpostModel.find({}).populate('comments').exec(function(err, results){
+    if(err){
+      res.send("failed");
+    }
+    res.json(results);
+  });
 });
 
 module.exports = app;
